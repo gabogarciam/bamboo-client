ac6adc48508f9cd4141b3da45bdb045f9ed3da9b
Gabriel Garcia <gabogarciam71@gmail.com>
2018-08-02 14:57:01 +0200

    client initialized and signup component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+
+const routes: Routes = [ { path: 'signup', component: SignupPageComponent} ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
